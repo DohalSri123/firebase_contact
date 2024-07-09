9d11437f6170e9b39de1a84617e8b11a342413e0
DohalSri123 <100751591+DohalSri123@users.noreply.github.com>
2024-07-09 14:56:26 +0530

    WV1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='my-4 bg-slate-600 h-[60px] rounded-lg flex items-center justify-center gap-4 text-xl font-medium'>
-      
-        <img className= ' h-12  w-12'src='./contact-icon.svg' />
         <h1 className=' text-white'>Contact App</h1>
     </div>
   )
